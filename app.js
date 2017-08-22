4604e83f1f53b6a918a9c67991ca3b8da5e67c0e
Matt Favoino <mattfavoino@gmail.com>
2017-08-21 17:24:14 -0700

    Added funciton that displays three pictures

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 'use strict';
 
 var allImageObjects = [];
+var previousImages = [];
+var currentImages = [];
 
-var recentImages = [];
-
-function Image(fileName, stringName, filePath, description) {
-  this.fileName = fileName;
+function Image(stringName, filePath, description) {
   this.stringName = stringName;
   this.filePath = filePath;
   this.desc = description;
   this.numShown = 0;
   this.numClicks = 0;
-  allImagesObjects.push(this);
+  allImageObjects.push(this);
   this.imgGenerator = function() {
-    //var ranNum = Math.floor(Math.random() * imagesArray.length - 1) + 1;
-    //var imgRandom = imagesArray[ranNum];
     var area = document.getElementById('photo_area');
     var img = document.createElement('img');
     img.setAttribute('alt', this.desc);
@@ -24,6 +21,21 @@ function Image(fileName, stringName, filePath, description) {
   };
 }
 
-var bag = new Image('bag.jpg', 'bag', 'assets/bag.jpg', 'A great looking bag');
-var banana = new Image('banana.jpg', 'banana', 'assets/banana.jpg', 'Banana slicer');
-var bathroom = new Image('bathroom.jpg', 'bathroom', 'assets/bathroom.jpg', 'Something for your bathroom');
+var bag = new Image('bag', 'assets/bag.jpg', 'A great looking bag.');
+var banana = new Image('banana', 'assets/banana.jpg', 'Banana slicer.');
+var bathroom = new Image('bathroom', 'assets/bathroom.jpg', 'Something for your bathroom.');
+var boots = new Image('boots', 'assets/boots.jpg', 'Sweet boots!');
+var breakfast = new Image('breatfast', 'assets/breakfast.jpg', 'Revolutionize your breakfast!');
+var bubblegum = new Image('bubblegum', 'assets/bubblegum.jpg', 'Delicious bubblegum.');
+var chair = new Image('bathroom', 'assets/chair.jpg', 'A comfortable chair.');
+var cthulhu = new Image('cthulhu', 'assets/cthulhu.jpg', 'A great statue.');
+
+function displayImages() {
+  for(var i = 0; i < 3; i++) {
+    var ranNum = Math.floor(Math.random() * allImageObjects.length - 1) + 1;
+    var placeholder = allImageObjects[ranNum].imgGenerator();
+    currentImages.push(placeholder);
+  }
+}
+
+displayImages();
