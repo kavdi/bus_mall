4f85867fc7338f28963af914f485c69573f54630
Matt Favoino <mattfavoino@gmail.com>
2017-08-21 19:57:24 -0700

    displayImages function working properly without repeats

--- app.js
+++ app.js
@@ -27,19 +27,28 @@ var bathroom = new Image('bathroom', 'assets/bathroom.jpg', 'Something for your 
 var boots = new Image('boots', 'assets/boots.jpg', 'Sweet boots!');
 var breakfast = new Image('breatfast', 'assets/breakfast.jpg', 'Revolutionize your breakfast!');
 var bubblegum = new Image('bubblegum', 'assets/bubblegum.jpg', 'Delicious bubblegum.');
-var chair = new Image('bathroom', 'assets/chair.jpg', 'A comfortable chair.');
+var chair = new Image('chair', 'assets/chair.jpg', 'A comfortable chair.');
 var cthulhu = new Image('cthulhu', 'assets/cthulhu.jpg', 'A great statue.');
+var dogduck = new Image('dog-duck', 'assets/dog-duck.jpg', 'A great looking dog-duck.');
+var dragon = new Image('dragon', 'assets/dragon.jpg', 'A great dragon.');
+var pen = new Image('pen', 'assets/pen.jpg', 'A great pen.');
 
-function displayImages() {
-  for(var i = 0; i < 3; i++) {
-    var ranNum = Math.floor(Math.random() * allImageObjects.length - 1) + 1;
-    if (currentImages.includes(ranNum)) {
-      break;
-    } else {
-      currentImages.push(ranNum);
-      allImageObjects[ranNum].imgGenerator();
-    }
-  } 
+function ranNum() {
+  return Math.floor(Math.random() * allImageObjects.length);
 };
 
+function displayImages() {
+  var currentImages = [];
+  for(var i = 0; i < 3; i++) {
+    var item = allImageObjects[ranNum()];
+    if (currentImages.includes(item) || previousImages.includes(item)) {
+      i--;
+      continue;
+    } else {
+      currentImages.push(item);
+      item.imgGenerator();
+    }
+  }
+  previousImages = currentImages;
+};
 displayImages();
